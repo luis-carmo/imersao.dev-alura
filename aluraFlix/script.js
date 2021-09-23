82af71e26a1955881a94c687526561191fcde450
luis-carmo <cunha.luis@live.com>
2021-09-23 17:46:33 -0300

    Adição do projeto 'AluraFlix'

---
+++ aluraFlix/script.js
@@ -0,0 +1,9 @@
+var listCharacters = [];
+
+
+function addCharacter() {
+  var characterLink = document.getElementById("link").value;
+  listCharacters.push(characterLink);
+  var body = document.body;
+  body.innerHTML += "<img src=" + characterLink + ">";
+}
