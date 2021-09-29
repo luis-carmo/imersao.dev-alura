3b4c049529f968d05d1139519014bf76e4ed784d
luis-carmo <cunha.luis@live.com>
2021-09-29 14:24:24 -0300

    Lista de desafios adicionados nos comentários

--- aluraFlix2/script.js
+++ aluraFlix2/script.js
@@ -16,3 +16,13 @@ function characterOnScreen (characterLink) {
   var screen = document.getElementById("characterOnScreen")
   screen.innerHTML += "<img src=" + characterLink + ">";
 }
+
+
+
+
+// DESAFIO 1: Criar um botão para remover um filme da lista.
+// DESAFIO 2: Colocar na tela o nome do filme, juntamente com o poster.
+// DESAFIO 3: Além de fazer os filmes aparecerem na tela, guardá-los em uma lista. 
+//            Desta forma, quando o botão "Adicionar Filme" for clicado, o programa imprime os filmes percorrendo a lista do zero.
+// DESAFIO 4: Praticar funções na aula do conversor de moedas. Fazer com que o programa devolva o valor para o usuário, utilizando a função return.
+// DESAFIO 5: Ainda na aula do conversor de moedas, utilizar radio buttons para que o usuário escolha para qual moeda ele quer fazer a conversão.
